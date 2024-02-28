8a3245669724cc7b52879036d1d33acfb3b31e3e
enrico-amerio <amerio.enrico95@gmail.com>
2024-02-28 17:31:05 +0100

    [fix] JS

--- Palidroma/js/script.js
+++ Palidroma/js/script.js
@@ -1,13 +1,26 @@
 const parola = prompt('inserisci una parola');
-console.log(palindroCheck(parola));
+// console.log(palindroCheck(parola));
+console.log(reverseString(parola));
 
 
 
-function palindroCheck(word) {
-  let alorap = word.split("").reverse().join("");
-  console.log(alorap);
-  if (alorap.toLowerCase() == parola.toLowerCase() ){
-    return 'Si è palindromo'
+// function palindroCheck(word) {
+//   let alorap = word.split("").reverse().join("");
+//   console.log(alorap);
+//   if (alorap.toLowerCase() == parola.toLowerCase() ){
+//     return 'Si è palindromo'
+//   }
+//   return 'No non è palindromo'
+// }
+
+function reverseString(word) {
+  var alorap = "";
+  for (var i = word.length - 1; i >= 0; i--) {
+      alorap += word[i];
   }
-  return 'No non è palindromo'
+  if (alorap.toLowerCase() == parola.toLowerCase() ){
+         return 'Si è palindromo'
+       }
+       return 'No non è palindromo'
+  
 }
--- Pari e dispari/js/script.js
+++ Pari e dispari/js/script.js
@@ -4,22 +4,21 @@ console.log(randomNum(1, 5));
 const totNum = parseInt(numeroUtente) + randomNum(1, 5);
 console.log(totNum);
 pariDispari(totNum);
-console.log(pariDispari());
-if (parDis === pariDispari()) {
-  console.log('vince Giocatore');
-  
+console.log(pariDispari(totNum));
+
+if(pariDispari(totNum) == false && parDis == 'dispari'){
+  console.log('vince il Giocatore');
+} else if (pariDispari(totNum) == true && parDis == 'pari') {
+  console.log('vince il Giocatore');
 }else{
   console.log('vince PC');
-
 }
-
+  
 
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 function pariDispari(num){
-  if(num % 2 == 0){
-    return 'pari'
-  }
-  return 'dispari'
+  return num % 2 == 0;
+  
 }
